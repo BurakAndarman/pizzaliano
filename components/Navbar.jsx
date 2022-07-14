7bcbab0d3c5c756d33c31f0de006b79e5548e37a
BurakAndarman <brkandrmn@hotmail.com>
2022-07-15 00:10:04 +0300

    finished home page responsive

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,26 +4,33 @@ import Image from 'next/image'
 
 const Navbar = () => {
   return (
-    <div className={styles.container}>
-      <div className={styles.item}>
-        <div className={styles.logo}>Pizzaliano</div>
+    <>
+      <div className={styles.container}>
+        <div className={styles.item}>
+          <div className={styles.logo}><a href="">Pizzaliano</a></div>
+        </div>
+        <div className={styles.item}>
+          <ul className={styles.list}>
+              <li className={styles.listItem}><a href="#">About</a></li>
+              <li className={styles.listItem}><a href="#">Events</a></li>
+              <li className={styles.listItem}><a href="#">Contact</a></li>
+          </ul>
+        </div>
+        <div className={styles.item}>
+          <div className={styles.cart}>   
+              <Image src="/img/cart.png" alt="" width="30px" height="30px"/>
+              <div className={styles.counter}>2</div>
+          </div>
+        </div>
       </div>
-      <div className={styles.item}>
-        <ul className={styles.list}>
-            <li className={styles.listItem}>Home</li>
-            <li className={styles.listItem}>Products</li>
-            <li className={styles.listItem}>About</li>
-            <li className={styles.listItem}>Contact</li>
+      <div className={styles.mobileItem}>
+        <ul className={styles.mobileList}> 
+          <li className={styles.mobileListItem}><a href="">About</a></li>
+          <li className={styles.mobileListItem}><a href="">Events</a></li>
+          <li className={styles.mobileListItem}><a href="">Contact</a></li>
         </ul>
       </div>
-      <div className={styles.item}>
-        <div className={styles.cart}>   
-            <Image src="/img/cart.png" alt="" width="30px" height="30px"/>
-            <div className={styles.counter}>2</div>
-        </div>
-
-      </div>
-    </div>
+    </>
   )
 }
 
